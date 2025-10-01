8c281c01d8bb27b3cf67069dded69e48af309738
Michael Danquah-Tabbi <mdanquahtabbi@gmail.com>
2025-09-30 20:30:24 -0400

    Added coin and game manager, updated player and level 1 scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 900.0
+const SPEED = 825.0
 const JUMP_VELOCITY = -2000.0
 
 
@@ -11,15 +11,33 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var run_multiplier = 1
+	
+	if Input.is_action_pressed("run"):
+		run_multiplier = 2
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		run_multiplier = 1
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED * run_multiplier
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * run_multiplier)
+		
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+		
+	if velocity.x != 0:
+		$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.play("idle")
+		
+
 
 	move_and_slide()
